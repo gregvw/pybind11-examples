d9703b7b360f30404f3ff7e2f21b0b10d856e10e
Greg von Winckel <gvonwin@sandia.gov>
2019-05-22 15:11:29 -0600

    Update

--- example/vector.cpp
+++ example/vector.cpp
@@ -2,6 +2,7 @@
 #include <pybind11/operators.h>
 #include <vector>
 #include <sstream>
+#include <iostream>
 
 namespace py = pybind11;
 
@@ -22,6 +23,12 @@ public:
     return result;
   }
 
+  Vector operator- ( const Vector& x ) const {
+    Vector result(data.size());
+    for(int i=0; i<data.size(); ++i)  result.data[i] = data[i] - x.data[i];
+    return result;
+  }
+
   inline int size() const { return static_cast<int>(data.size()); }
   inline const_reference operator[] ( int i ) const noexcept { return data[i]; }
   inline reference       operator[] ( int i )       noexcept { return data[i]; }
@@ -34,6 +41,22 @@ public:
      return s.str();
    } 
  
+   void apply_function( py::object func ) {
+     for( auto& e : data ) {
+       py::object result = func(e);
+       e = result.cast<T>();
+     }
+   }
+
+   void reduce( py::object r ) {
+     py::float_ minus_one(-1.0), one(1.0), two(2.0), zero(0.0);
+     py::float_ y = r(-one-one,-one);
+     py::float_ x = r(zero,one);
+     std::cout << x.cast<T>() << " " << y.cast<T>() << std::endl;
+     
+   }
+
+
 private: 
 
   data_type data;
@@ -55,9 +78,17 @@ py::class_<Vector<T>> pybind_class_Vector( py::module& m, const char* name ) {
     if(index>v.size()) throw py::index_error();
     else return v[index];
   })
+  .def("__len__",&Vector<T>::size)
   .def("__str__",&Vector<T>::to_string)
   .def("__repr__",&Vector<T>::to_string)
-  .def(py::self + py::self);
+  .def("apply_function",[]( Vector<T>& v, py::object func ) {
+    v.apply_function(func);
+   })
+  .def("reduce",[]( Vector<T>& v, py::object r ) { 
+    v.reduce(r);
+   })
+  .def(py::self + py::self)
+  .def(py::self - py::self);
 }
 
 
