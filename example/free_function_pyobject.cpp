4ae4c7347a4c1d9b5d6e5214d88d3b38c0c05b49
Greg von Winckel <gregvw@gmail.com>
2019-05-24 21:37:27 -0600

    Update

--- example/free_function_pyobject.cpp
+++ example/free_function_pyobject.cpp
@@ -4,11 +4,11 @@
 namespace py = pybind11;
 
 
-py::object add( py::object x,  py::object y ) {
+py::list concatenate( py::list x,  py::list y ) {
   return x+y;
 }
 
 PYBIND11_MODULE(free_function_pyobject,m) {
   m.doc() = "Example of using simple free function with PyObjects";
-  m.def("add", &add, "Adds two Python Objects");
+  m.def("concatenate", &concatenate, "Concatenate two Python lists");
 }
--- example/vector.cpp
+++ example/vector.cpp
@@ -48,20 +48,35 @@ public:
      }
    }
 
-   void reduce( py::object r ) {
-     py::float_ minus_one(-1.0), one(1.0), two(2.0), zero(0.0);
-     py::float_ y = r(-one-one,-one);
-     py::float_ x = r(zero,one);
-     std::cout << x.cast<T>() << " " << y.cast<T>() << std::endl;
-     
-   }
+   T reduce( py::object r ) {
 
+     // Deduce function type as min, max, plus, or multiplies
+     py::float_ minus_one(-1.0), zero(0.0), one(1.0);
+
+     py::float_ test_1 = r(minus_one,one);  T value_1 = test_1.cast<T>();
+     py::float_ test_2 = r(minus_one,zero); T value_2 = test_2.cast<T>();
+     T test_value = 3 + 2*value_1 + value_2;
+
+     py::float_ result;
+
+     if     ( test_value == T(0) ) result = py::float_(std::numeric_limits<T>::max());
+     else if( test_value == T(5) ) result = py::float_(std::numeric_limits<T>::min());
+     else if( test_value == T(2) ) result = zero;
+     else if( test_value == T(1) ) result = one;
+     else throw std::runtime_error("Binary function could not be identified");
+
+     for( auto& e : data ) result = r(result,e);
+
+     return result.cast<T>();       
+   }
 
 private: 
 
   data_type data;
 
 }; // class Vector
+
+
 
 template<typename T>
 py::class_<Vector<T>> pybind_class_Vector( py::module& m, const char* name ) {
@@ -85,7 +100,7 @@ py::class_<Vector<T>> pybind_class_Vector( py::module& m, const char* name ) {
     v.apply_function(func);
    })
   .def("reduce",[]( Vector<T>& v, py::object r ) { 
-    v.reduce(r);
+    return v.reduce(r);
    })
   .def(py::self + py::self)
   .def(py::self - py::self);
